6f8b02859ea8120a6dd6f2919686b625e852afe1
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-12-13 02:23:31 +0530

    Serializing the data from DB in DJango and implemented Category API routing and views in Django

--- api/category/models.py
+++ api/category/models.py
@@ -6,4 +6,7 @@ class Category(models.Model):
     description = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
     
