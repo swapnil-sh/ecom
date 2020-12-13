a4e820b07c00e397860e644fde2dbbed70a0b702
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-12-13 17:02:56 +0530

    setting up app Product adding models and basic stuffs

--- api/product/views.py
+++ api/product/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('id')
+    serializer_class = ProductSerializer
